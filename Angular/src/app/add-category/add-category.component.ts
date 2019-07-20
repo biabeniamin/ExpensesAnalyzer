b33b31c060b680ebe65af6dc07c35ea071b6fcc6
Beniamin Bia <biabeniamin@outlook.com>
2019-07-20 10:39:48 +0300

    add category added

---
+++ Angular/src/app/add-category/add-category.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CategorieService } from '../Controllers/CategorieService';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.scss']
+})
+export class AddCategoryComponent implements OnInit {
+
+  constructor(private categorieService : CategorieService
+    ) { }
+
+  ngOnInit() {
+  }
+
+}
